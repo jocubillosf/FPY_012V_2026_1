8d17824db7b399e28cfc977e9471aff0ecfa4af5
jmreyesc <jm.reyesc@duocuc.cl>
2026-06-23 17:02:10 -0400

    Se corrigen los equipos por parte de los grupos, y la funcion verpartidos para que se muestren con las modificiaciones

--- funciones.py
+++ funciones.py
@@ -1,22 +1,25 @@
 import equipos 
 
-partidos = [
-    (equipos.team_portugal,equipos.team_uzbekistan),
-    (equipos.team_inglaterra,equipos.team_ghana),
-    (equipos.team_panama,equipos.team_croacia),
-    (equipos.team_colombia,equipos.team_congo)
-    ]
+partidos = {
+    "Grupo K": [(equipos.team_portugal,equipos.team_uzbekistan)],
+    "Grupo D": [(equipos.team_inglaterra,equipos.team_ghana)],
+    "Grupo F": [(equipos.team_panama,equipos.team_croacia)],
+    "Grupo H": [(equipos.team_colombia,equipos.team_congo)]
+}
 
 def verpartidos(partidos):
 
-    for team1, team2 in partidos:
-        print(team1["nombre"], "vs",team2["nombre"])
-        print(team1)
-        print(team2)
+    for grupo, parti in partidos.items():
+        print(f"\n {grupo}")
+        print("="* 40)
+        for team1, team2 in parti:
+            print(team1["nombre"], "vs",team2["nombre"])
+            print(team1)
+            print(team2)
 
-def apostar():
+def apostar(partidos):
+    
 
 
 def veresumen():
 
-    
