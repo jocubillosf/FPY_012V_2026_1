d5199bc95e90eca9e0e40053138b97925ee0cf78
jmreyesc <jm.reyesc@duocuc.cl>
2026-06-23 16:02:52 -0400

    Se crea archivo con equipos de futbol

---
+++ equipos.py
@@ -0,0 +1,111 @@
+team_Portugal = {
+    "nombre":"Portugal",
+    "remates":16,
+    "remates al arco":9,
+    "posesion":"66%",
+    "pases":606,
+    "precision de los pases":"92%",
+    "faltas":14,
+    "tarjetas amarillas":1,
+    "tarjetas rojas":0,
+    "posicion adelantada":2,
+    "tiros de esquina":3
+}
+
+team_Uzbekistan = {
+    "nombre":"Uzbekistán",
+    "remates":7,
+    "remates al arco":2,
+    "posesion":"34%",
+    "pases":263,
+    "precision de los pases":"74%",
+    "faltas":15,
+    "tarjetas amarillas":1,
+    "tarjetas rojas":0,
+    "posicion adelantada":2,
+    "tiros de esquina":2
+}
+
+team_Inglaterra = {
+    "nombre":"Inglaterra",
+    "remates":14,
+    "remates al arco":6,
+    "posesion":"61%",
+    "pases":487,
+    "precision de los pases":"85%",
+    "faltas":11,
+    "tarjetas amarillas":2,
+    "tarjetas rojas":0,
+    "posicion adelantada":3,
+    "tiros de esquina":5
+}
+
+team_ghana = {
+    "nombre":"Ghana",
+    "remates":8,
+    "remates al arco":3,
+    "posesion":"39%",
+    "pases":301,
+    "precision de los pases":"71%",
+    "faltas":13,
+    "tarjetas amarillas":1,
+    "tarjetas rojas":0,
+    "posicion adelantada":1,
+    "tiros de esquina":2
+}
+
+team_panama = {
+    "nombre":"Panama",
+    "remates":6,
+    "remates al arco":2,
+    "posesion":"38%",
+    "pases":278,
+    "precision de los pases":"69%",
+    "faltas":16,
+    "tarjetas amarillas":3,
+    "tarjetas rojas":0,
+    "posicion adelantada":2,
+    "tiros de esquina":3
+}
+
+team_croacia = {
+    "nombre":"Croacia",
+    "remates":13,
+    "remates al arco":5,
+    "posesion":"62%",
+    "pases":453,
+    "precision de los pases":"83%",
+    "faltas":10,
+    "tarjetas amarillas":1,
+    "tarjetas rojas":0,
+    "posicion adelantada":3,
+    "tiros de esquina":6
+}
+
+team_colombia = {
+    "nombre":"Colombia",
+    "remates":12,
+    "remates al arco":5,
+    "posesion":"55%",
+    "pases":412,
+    "precision de los pases":"80%",
+    "faltas":12,
+    "tarjetas amarillas":2,
+    "tarjetas rojas":0,
+    "posicion adelantada":4,
+    "tiros de esquina":4
+}
+
+team_congo = {
+    "nombre":"Congo DR",
+    "remates":9,
+    "remates al arco":4,
+    "posesion":"45%",
+    "pases":334,
+    "precision de los pases":"76%",
+    "faltas":14,
+    "tarjetas amarillas":2,
+    "tarjetas rojas":1,
+    "posicion adelantada":2,
+    "tiros de esquina":3                                                                                                                                                                                                                                                                                                                                                                                                                                
+}
